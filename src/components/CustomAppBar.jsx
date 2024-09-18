9ff02de36e48dc46234df607fbbd80ff8217ff66
Tristan Rosales <tristanrosales0@gmail.com>
2024-09-18 23:36:51 +0800

    Remove drawer & include topbar header only

--- src/components/CustomAppBar.jsx
+++ src/components/CustomAppBar.jsx
@@ -2,159 +2,25 @@ import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemText from "@mui/material/ListItemText";
-import Box from "@mui/material/Box";
-import Drawer from "@mui/material/Drawer";
-import Avatar from '@mui/material/Avatar';
-import List from "@mui/material/List";
-import Divider from "@mui/material/Divider";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import { useNavigate } from "react-router";
-import { useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import ChevronRightIcon from '@mui/icons-material/ChevronRightOutlined';
-import ChevronLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
-import { styled, useTheme } from '@mui/material/styles';
-import { Home } from "@mui/icons-material";
+import { styled } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import AppLogoImg from '../assets/images/app-logo.png';
+import { Box } from "@mui/material";
 
-const drawerWidth = 300;
-
-const openedMixin = (theme) => ({
-    width: drawerWidth,
-    transition: theme.transitions.create('width', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.enteringScreen,
-    }),
-    overflowX: 'hidden',
-});
-
-const closedMixin = (theme) => ({
-    transition: theme.transitions.create('width', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-    }),
-    overflowX: 'hidden',
-    width: `calc(${theme.spacing(7)} + 1px)`,
-    [theme.breakpoints.up('sm')]: {
-        width: `calc(${theme.spacing(8)} + 1px)`,
-    },
-});
-
-const DrawerHeader = styled('div')(({ theme }) => ({
-    display: 'flex',
-    background: '#1976d2',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    padding: theme.spacing(0, 1),
-    ...theme.mixins.toolbar,
-}));
-
-const AppBarStyled = styled(AppBar, {
-    shouldForwardProp: (prop) => prop !== 'open',
-})(({ theme, open }) => ({
+const AppBarStyled = styled(AppBar)(({ theme }) => ({
     zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(['width', 'margin'], {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-    }),
-    ...(open && {
-        marginLeft: drawerWidth,
-        width: `calc(100% - ${drawerWidth}px)`,
-        transition: theme.transitions.create(['width', 'margin'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-    }),
 }));
-
-const DrawerStyled = styled(Drawer, { shouldForwardProp: (prop) => prop !== 'open' })(
-    ({ theme, open }) => ({
-        width: drawerWidth,
-        flexShrink: 0,
-        whiteSpace: 'nowrap',
-        boxSizing: 'border-box',
-        ...(open && {
-            ...openedMixin(theme),
-            '& .MuiDrawer-paper': openedMixin(theme),
-        }),
-        ...(!open && {
-            ...closedMixin(theme),
-            '& .MuiDrawer-paper': closedMixin(theme),
-        }),
-    }),
-);
 
 export default function CustomAppBar() {
-    const [open, setOpen] = React.useState(true);
-    const theme = useTheme();
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-
-    const handleDrawerOpen = () => {
-        setOpen(true);
-    };
-
-    const handleDrawerClose = () => {
-        setOpen(false);
-    };
-
-    const itemList = [
-        { text: "Home", icon: <Home />, link: "/home" }
-    ];
-
     return (
         <Box sx={{ display: 'flex' }}>
             <CssBaseline />
-            <AppBarStyled position="fixed" open={open}>
+            <AppBarStyled position="fixed">
                 <Toolbar>
-                    <IconButton
-                        color="inherit"
-                        aria-label="open drawer"
-                        onClick={handleDrawerOpen}
-                        edge="start"
-                        sx={{
-                            marginRight: 5,
-                            ...(open && { display: 'none' }),
-                        }}
-                    >
-                        <MenuIcon />
-                    </IconButton>
                     <Typography variant="h6" noWrap component="div">
                         SWAPI App
                     </Typography>
                 </Toolbar>
             </AppBarStyled>
-            <DrawerStyled variant="permanent" open={open}>
-                <DrawerHeader>
-                    <Avatar alt="SWAPI Logo" src={AppLogoImg} />
-                    <IconButton color="inherit" onClick={handleDrawerClose}>
-                        {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon style={{ color: 'white' }} />}
-                    </IconButton>
-                </DrawerHeader>
-
-                <Divider />
-                <List>
-                    {itemList.map((item) => (
-                        <ListItem key={item.text} disablePadding style={{ display: 'block' }}>
-                            <Link to={item.link} style={{ textDecoration: "none", color: "inherit" }}>
-                                <ListItemButton>
-                                    <ListItemIcon>
-                                        {item.icon}
-                                    </ListItemIcon>
-                                    <ListItemText primary={item.text} />
-                                </ListItemButton>
-                            </Link>
-                        </ListItem>
-                    ))}
-                </List>
-                <Divider />
-            </DrawerStyled>
         </Box>
     );
 }
